a9f73e7781c6ff11123c585d090cfa9c84ef9dca
Jezhora <jezhora.photo@gmail.com>
2023-05-31 13:57:06 +0300

    feat: add close mobile menu indexJS

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	// hamburger
 	const menu = document.querySelector('.header__menu'),
 		menuItem = document.querySelectorAll('.header__menu_item'),
-		hamburger = document.querySelector('.hamburger');
+		hamburger = document.querySelector('.hamburger'),
+		sections = document.querySelectorAll('section');
 
 	hamburger.addEventListener('click', () => {
 		hamburger.classList.toggle('hamburger_active');
@@ -13,6 +14,14 @@ window.addEventListener('DOMContentLoaded', () => {
 		item.addEventListener('click', () => {
 			hamburger.classList.toggle('hamburger_active');
 			menu.classList.toggle('header__menu_active');
+		});
+	});
+
+	// close mobile menu
+	sections.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.remove('hamburger_active');
+			menu.classList.remove('header__menu_active');
 		});
 	});
 
