7c7a5508994c98c18e6c03dec357240a600c656f
Jezhora <jezhora.photo@gmail.com>
2023-06-01 14:32:44 +0300

    feat: add data to HTML and service blur done

--- js/index.js
+++ js/index.js
@@ -27,89 +27,68 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	// pick service --> blur
 	const serviceItems = document.querySelectorAll('.service__item'),
-		  serviceBtnsParents = document.querySelector('.service__btns');
+		  serviceBtnsParents = document.querySelector('.service__btns'),
+		  serviceGardens = document.querySelector('[data-gardens]'),
+		  serviceLawn = document.querySelector('[data-lawn]'),
+		  servicePlanting = document.querySelector('[data-planting]');
 
+	let count = 0;
 
 	serviceBtnsParents.addEventListener('click', (event) => {
 		const target = event.target;
-		
-		if(target.classList.contains('btn__service')) {
-			target.classList.add('btn__service_active')
-			target.classList.remove('btn__service')
-			addBlur(target);
-		} else if(target.classList.contains('btn__service_active')) {
-			target.classList.add('btn__service')
-			target.classList.remove('btn__service_active')
-			removeBlur(target);
-		}
-	})
 
-	function addBlur(target) {
-		if (target.classList.contains('btn__service_active')){
-			if(target.classList.contains('gardens')) {
-				serviceItems.forEach(item =>{
-					if(item.classList.contains('gardens')) {
+				if(target.classList.contains('btn__service') && count < 2) {
+					target.classList.add('btn__service_active')
+					target.classList.remove('btn__service')
+					serviceItems.forEach(item => {
 						item.classList.add('blur')
-					}
-				})
-			} else if(target.classList.contains('planting')) {
-				serviceItems.forEach(item =>{
-					if(item.classList.contains('planting')) {
+					})
+					removeBlur()
+					count ++;
+				} else if (target.classList.contains('btn__service_active')) {
+					target.classList.add('btn__service')
+					target.classList.remove('btn__service_active')
+					serviceItems.forEach(item => {
 						item.classList.add('blur')
-					}
-				})
-			} else if(target.classList.contains('lawn')) {
-				serviceItems.forEach(item =>{
-					if(item.classList.contains('lawn')) {
-						item.classList.add('blur')
-					}
-				})
-			}	
-		}
-	}
-
-	function removeBlur(target) {
-		if (target.classList.contains('btn__service')){
-			if(target.classList.contains('gardens')) {
-				serviceItems.forEach(item =>{
-					if(item.classList.contains('gardens')) {
+					})
+					removeBlur()
+					count --;
+				}
+				if (count === 0) {
+					serviceItems.forEach(item => {
 						item.classList.remove('blur')
-					}
-				})
-			} else if(target.classList.contains('planting')) {
-				serviceItems.forEach(item =>{
-					if(item.classList.contains('planting')) {
-						item.classList.remove('blur')
-					}
-				})
-			} else if(target.classList.contains('lawn')) {
-				serviceItems.forEach(item =>{
-					if(item.classList.contains('lawn')) {
-						item.classList.remove('blur')
-					}
-				})
-			}	
-		}
-	}
-
-	
-	// serviceBtns.forEach((item, i) => {
-	// 	item.addEventListener('click', () => {
+					})
+				}
 			
-	// 		item.classList.toggle('btn__service_active');
-	// 		addBlur()
-	
-	// 	})
-	// })
+		function removeBlur() {	
 
+			serviceGardens.classList.forEach(item => {
+				if(item === 'btn__service_active'){		
+				serviceItems.forEach(item =>{
+					if(item.classList.contains('gardens'))	
+					item.classList.remove('blur')	
+					})
+				}
+			})
+			serviceLawn.classList.forEach(item => {
+				if(item === 'btn__service_active'){		
+				serviceItems.forEach(item =>{
+					if(item.classList.contains('lawn'))	
+					item.classList.remove('blur')	
+					})
+				}
+			})
+			servicePlanting.classList.forEach(item => {
+				if(item === 'btn__service_active'){		
+				serviceItems.forEach(item =>{
+					if(item.classList.contains('planting'))	
+					item.classList.remove('blur')	
+					})
+				}
+			})
+		}
+	});
 	
-
-
-	
-	
-
-	
-
 
 });
 
